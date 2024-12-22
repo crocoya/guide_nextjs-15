3ec2ceb4416da048a547a5c8144554e7290d9690
crocoya <yassine.tababi@outlook.com>
2024-12-22 17:32:16 +0100

    feat: add metadata

--- attachments/05-onwards-foodies-starting-project/lib/action.js
+++ attachments/05-onwards-foodies-starting-project/lib/action.js
@@ -2,12 +2,13 @@
 
 import { redirect } from 'next/navigation';
 import { saveMeal } from './meals';
+import { revalidatePath } from 'next/cache';
 
 function isInvalidText(text) {
   return !text || text.trim() === '';
 }
 
-export async function shareMeal(formData) {
+export async function shareMeal(preState, formData) {
   const meal = {
     title: formData.get('title'),
     summary: formData.get('summary'),
@@ -27,9 +28,12 @@ export async function shareMeal(formData) {
     !meal.image ||
     meal.image.size === 0
   ) {
-    throw new Error('Invalid input');
+    return {
+      message: 'Invalid input.',
+    };
   }
 
   await saveMeal(meal);
+  revalidatePath('/meals');
   redirect('/meals');
 }
